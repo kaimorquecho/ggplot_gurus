e1d69b3c114aa8d5cb074def70c582433b16d6e6
ryangreen6 <ryangreen@bren.ucsb.edu>
2024-08-28 21:37:45 +0000

    changes for is.na TRUE

--- data_read.R
+++ data_read.R
@@ -2,10 +2,12 @@ library(tidyverse)
 
 ggg_path <- "/courses/EDS214/group_project/2024/ggplot-gurus/raw_data"
 
-bq1 <- read_csv(file.path(ggg_path, "QuebradaCuenca1-Bisley.csv"))
+bq1 <- read_csv(file.path(ggg_path, "QuebradaCuenca1-Bisley.csv"), is.na(TRUE))
   
-bq2 <- read_csv(file.path(ggg_path, "QuebradaCuenca2-Bisley.csv"))
+bq2 <- read_csv(file.path(ggg_path, "QuebradaCuenca2-Bisley.csv"), is.na(TRUE))
   
-bq3 <- read_csv(file.path(ggg_path, "QuebradaCuenca3-Bisley.csv"))
+bq3 <- read_csv(file.path(ggg_path, "QuebradaCuenca3-Bisley.csv"), is.na(TRUE))
 
-prm <- read_csv(file.path(ggg_path, "RioMameyesPuenteRoto.csv"))
+prm <- read_csv(file.path(ggg_path, "RioMameyesPuenteRoto.csv"), is.na(TRUE))
+                
+
