417a716a2b279fecaa48b8f3098d00714e66b46f
kaimorquecho <kaimorquecho@ucsb.edu>
2024-08-28 23:05:29 +0000

    basic ggplot + geom_line script

--- data_viz.R
+++ data_viz.R
@@ -1,9 +1,12 @@
-ggplot() + 
+
+k_plot <- ggplot() + 
   geom_line(data= bq1_tidy, aes(x= sample_date, y = k), color='steelblue') + 
-  geom_line(data= bq1_tidy, aes(x= sample_date, y = no3_n), color='') +
-  geom_line(data= bq2_tidy, aes(x= sample_date, y = k), color='') +
-  geom_line(data= bq2_tidy, aes(x= sample_date, y = no3_n), color='') +
-  geom_line(data= bq3_tidy, aes(x= sample_date, y = k), color='') +
-  geom_line(data= bq3_tidy, aes(x= sample_date, y = no3_n), color='') +
-  geom_line(data= prm_tidy, aes(x= sample_date, y = k ), color='') +
-  geom_line(data= prm_tidy, aes(x= sample_date, y = no3_n), color='') 
+  geom_line(data= prm_tidy, aes(x= sample_date, y = k ), color='green') +
+  geom_line(data= bq2_tidy, aes(x= sample_date, y = k), color='yellow') +
+  geom_line(data= bq3_tidy, aes(x= sample_date, y = k), color='red') 
+
+no3_n <- ggplot() +  
+  geom_line(data= bq2_tidy, aes(x= sample_date, y = no3_n), color='yellow') +
+  geom_line(data= bq3_tidy, aes(x= sample_date, y = no3_n), color='red') +
+  geom_line(data= prm_tidy, aes(x= sample_date, y = no3_n), color='green') +
+  geom_line(data= bq1_tidy, aes(x= sample_date, y = no3_n), color='steelblue')
