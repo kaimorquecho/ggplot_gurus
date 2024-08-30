9a2e005f91c6dfd6da0adbcb66272b6dee2982fb
ryangreen6 <ryangreen@bren.ucsb.edu>
2024-08-30 17:58:48 +0000

    increased line size by .5

--- hurricane_maria.R
+++ hurricane_maria.R
@@ -58,10 +58,10 @@ prm_tidy_maria <- prm_tidy_maria %>%
 #8A89C0
 
 k_plot_maria <- ggplot() + 
-  geom_line(data= bq1_tidy_maria, aes(x= sample_date, y = k, color="BQ1"), size=1) + 
-  geom_line(data= bq2_tidy_maria, aes(x= sample_date, y = k, color="BQ2"), size=1) +
-  geom_line(data= bq3_tidy_maria, aes(x= sample_date, y = k, color="BQ3"), size=1) +
-  geom_line(data= prm_tidy_maria, aes(x= sample_date, y = k, color="PRM"), size=1) +
+  geom_line(data= bq1_tidy_maria, aes(x= sample_date, y = k, color="BQ1"), size=1.5) + 
+  geom_line(data= bq2_tidy_maria, aes(x= sample_date, y = k, color="BQ2"), size=1.5) +
+  geom_line(data= bq3_tidy_maria, aes(x= sample_date, y = k, color="BQ3"), size=1.5) +
+  geom_line(data= prm_tidy_maria, aes(x= sample_date, y = k, color="PRM"), size=1.5) +
   scale_color_manual(values = c("BQ1" = "#048A81", "BQ2" = "#06D6A0", "BQ3" = "#54C6EB", "PRM" = "#8A89C0")) +
   geom_vline(xintercept = as.Date("2017-09-16"), linetype = "dashed", color="navy") +
   annotate("text", x=as.Date("2017-11-05"), y=1.9, label= "Hurricane Maria\n09-16-2017") +
@@ -77,11 +77,13 @@ k_plot_maria <- ggplot() +
 #####
 
 no3_n_maria <- ggplot() +  
-  geom_line(data = bq1_tidy_maria, aes(x = sample_date, y = no3_n, color = "BQ1"), size = 1) +
-  geom_line(data = bq2_tidy_maria, aes(x = sample_date, y = no3_n, color = "BQ2"), size = 1) +
-  geom_line(data = bq3_tidy_maria, aes(x = sample_date, y = no3_n, color = "BQ3"), size = 1) +
-  geom_line(data = prm_tidy_maria, aes(x = sample_date, y = no3_n, color = "PRM"), size = 1) +
+  geom_line(data = bq1_tidy_maria, aes(x = sample_date, y = no3_n, color = "BQ1"), size = 1.5) +
+  geom_line(data = bq2_tidy_maria, aes(x = sample_date, y = no3_n, color = "BQ2"), size = 1.5) +
+  geom_line(data = bq3_tidy_maria, aes(x = sample_date, y = no3_n, color = "BQ3"), size = 1.5) +
+  geom_line(data = prm_tidy_maria, aes(x = sample_date, y = no3_n, color = "PRM"), size = 1.5) +
   scale_color_manual(values = c("BQ1" = "#048A81", "BQ2" = "#06D6A0", "BQ3" = "#54C6EB", "PRM" = "#8A89C0")) +
+  geom_vline(xintercept = as.Date("2017-09-16"), linetype = "dashed", color="navy") +
+  annotate("text", x=as.Date("2017-07-26"), y=700, label= "Hurricane Maria\n09-16-2017") +
   labs(color = "Sites") +
   theme(legend.position = c(0.2, 0.8),
         legend.background = element_blank(),
