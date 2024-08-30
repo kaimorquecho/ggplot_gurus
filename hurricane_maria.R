be258aa46209e5ff132c4b5175476b8950c5e13c
ryangreen6 <ryangreen@bren.ucsb.edu>
2024-08-30 17:55:11 +0000

    hurricane maria more final plots

--- hurricane_maria.R
+++ hurricane_maria.R
@@ -49,6 +49,7 @@ prm_tidy_maria <- prm_tidy_maria %>%
   filter(between(sample_date, as.Date('2016-01-01'), as.Date('2021-12-31')))
 
 
+
 # data_viz ----------------------------------------------------------------
 
 #048A81
@@ -62,10 +63,18 @@ k_plot_maria <- ggplot() +
   geom_line(data= bq3_tidy_maria, aes(x= sample_date, y = k, color="BQ3"), size=1) +
   geom_line(data= prm_tidy_maria, aes(x= sample_date, y = k, color="PRM"), size=1) +
   scale_color_manual(values = c("BQ1" = "#048A81", "BQ2" = "#06D6A0", "BQ3" = "#54C6EB", "PRM" = "#8A89C0")) +
+  geom_vline(xintercept = as.Date("2017-09-16"), linetype = "dashed", color="navy") +
+  annotate("text", x=as.Date("2017-11-05"), y=1.9, label= "Hurricane Maria\n09-16-2017") +
   labs(color = "Sites") +
-  theme(legend.position = c(0.2, 0.8),
-        legend.background = element_blank())
+  theme(legend.position = c(0.25, 0.8),
+        legend.background = element_blank(),
+        legend.key.size = unit(10, 'cm'),
+        legend.key.height  = unit(1, 'cm'),
+        legend.key.width = unit(4, 'cm'),
+        plot.title = element_text(hjust = 0.5, size= 25)) +
+  labs(x="Sample Date", y="Nitrate Content", title = "Effects of Hurricane Maria on Potassium Content in Luquillo Creeks")
  
+#####
 
 no3_n_maria <- ggplot() +  
   geom_line(data = bq1_tidy_maria, aes(x = sample_date, y = no3_n, color = "BQ1"), size = 1) +
@@ -75,8 +84,15 @@ no3_n_maria <- ggplot() +
   scale_color_manual(values = c("BQ1" = "#048A81", "BQ2" = "#06D6A0", "BQ3" = "#54C6EB", "PRM" = "#8A89C0")) +
   labs(color = "Sites") +
   theme(legend.position = c(0.2, 0.8),
-        legend.background = element_blank())
+        legend.background = element_blank(),
+        legend.key.size = unit(10, 'cm'),
+        legend.key.height  = unit(1, 'cm'),
+        legend.key.width = unit(4, 'cm'),
+        plot.title = element_text(hjust = 0.5, size= 25)) +
+  labs(x="Sample Date", y="Nitrate Content", title = "Effects of Hurricane Maria on Nitrate Content in Luquillo Creeks")
 
+
+######
 
 k_plot_maria
 no3_n_maria
